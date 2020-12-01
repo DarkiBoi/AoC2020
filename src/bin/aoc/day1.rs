8e9724af566bce533df00697c84e1cc763c14fe7
Darki <50775247+darkiboi@users.noreply.github.com>
2020-12-01 16:50:28 +0100

    Day 1 Part Two

--- src/bin/aoc/day1.rs
+++ src/bin/aoc/day1.rs
@@ -1,6 +1,11 @@
 mod util;
 
 fn main() {
+    part_one();
+    part_two();
+}
+
+pub fn part_one() {
     let lines: Vec<i32> = util::get_input_lines().map(|s| s.parse::<i32>().unwrap()).collect();
 
     for i in 0..lines.len() {
@@ -11,9 +16,35 @@ fn main() {
             }
             let second = lines[p];
             if(first + second) == 2020 {
-                println!("Result found! {}", first * second);
+                println!("Part One: {}", first * second);
                 return;
             }
         }
     }
+}
+
+pub fn part_two() {
+    let lines: Vec<i32> = util::get_input_lines().map(|s| s.parse::<i32>().unwrap()).collect();
+
+    for i in 0..lines.len() {
+        let first = lines[i];
+        for p in  0..lines.len() {
+            if p == i {
+                continue
+            }
+            let second = lines[p];
+            for q in 0..lines.len() {
+                if q == p || q == i {
+                    continue
+                }
+                let third = lines[q];
+                if(first + second + third) == 2020 {
+                    println!("Part Two: {}", first * second * third);
+                    return;
+                }
+            }
+
+        }
+    }
+
 }
